8b952fdd65c25232a950cf4ecfa328a1e4df5596
Oana Muntean <oanamuntean8@gmail.com>
2016-11-07 19:48:30 +0200

    Changed home and project routes.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,6 @@
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
-import { ModuleWithProviders } from '@angular/core';
+import {ModuleWithProviders} from '@angular/core';
 import {AboutComponent} from "./about/about.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {ProjectListComponent} from "./projects/project-list/project-list.component";
@@ -12,18 +12,18 @@ import {LoginComponent} from "./login/login.component";
 import {AuthGuard} from "./core/auth-guard.service";
 
 
-const appRoutes:Routes = [
-    {path: '', component: ProjectListComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'home', component: HomeComponent},
-    {path: 'project/:id', component: ProjectComponent},
-    {path: 'start-project', component: AddProjectComponent, canActivate: [AuthGuard]},
-    {path: 'profile/:id', component: ProfileComponent},
-    {path: 'settings', component: SettingsComponent, canActivate: [AuthGuard]},
-    {path: 'auth/:type/callback', component: LoginComponent},
-    {path: '**', component: NotFoundComponent}
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'projects', component: ProjectListComponent},
+  {path: 'project/:id', component: ProjectComponent},
+  {path: 'start-project', component: AddProjectComponent, canActivate: [AuthGuard]},
+  {path: 'profile/:id', component: ProfileComponent},
+  {path: 'settings', component: SettingsComponent, canActivate: [AuthGuard]},
+  {path: 'auth/:type/callback', component: LoginComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
-export const appRoutingProviders:any[] = [];
+export const appRoutingProviders: any[] = [];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
