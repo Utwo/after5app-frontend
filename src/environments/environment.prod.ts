ea05135999b2181d6c6ddd080fa49e2e2a9e6fe6
Utwo <mihai.legat@gmail.com>
2016-11-08 16:05:58 +0200

    Add .htaccess. Change env variables.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   URL: 'http://after5app.co/',
-  URL_API: 'http://after5app.co/',
+  URL_API: 'http://api.after5app.co/',
   API_VERSION: 'api/v1/',
   FACEBOOK_ID: '1614649558855314',
   GITHUB_ID: '4b122575498973d10891'
