6552ee58d106af2f75a3b6bde5145bc861903b4a
Oana Muntean <oanamuntean8@gmail.com>
2017-03-08 15:39:33 +0200

    Feebe info component final design

--- src/app/shared/components/feebe-info.component.ts
+++ src/app/shared/components/feebe-info.component.ts
@@ -3,18 +3,22 @@ import {Component, OnInit, Input} from '@angular/core';
 @Component({
   selector: 'app-febee-info',
   template: `
-    <div class="row my-3">
-      <div class="col-1 feebe-info__image">
-        <div class="feebe-head">
+    <div class="row no-gutters my-3">
+      <div class="col-md-1 col-2">
+      <div class="feebe-info__image">
+        <div class="feebe-head mx-auto">
           <span class="febee-eyes"></span>
         </div>
         Feebe
       </div>
-      <div class="col-11 feebe-info__message">
-        <p><b>{{title}}</b></p>
-        <p> {{description}}</p>
       </div>
-     </div>
+      <div class="col-md-11 col-10">
+        <div class="feebe-info__message">
+          <p><b>{{title}}</b></p>
+          <p> {{description}}</p>
+        </div>
+      </div>
+    </div>
 `
 })
 export class FeebeInfoComponent implements OnInit {
