a99dc9091d1907c1a7d8a44ce074ff7b3a58d827
Eliza <eliza.nitoi@gmail.com>
2017-03-14 23:26:49 +0200

    Ended share modal. Prepared applicants component

--- src/app/projects/project-detail/project-applicants/applications-card/applications-card.component.ts
+++ src/app/projects/project-detail/project-applicants/applications-card/applications-card.component.ts
@@ -7,45 +7,34 @@ import {ResponseHandlerService} from '../../../../shared/response-handler.servic
   templateUrl: 'applications-card.component.html'
 })
 export class ApplicationsCardComponent implements OnInit {
-  @Input('project_id') project_id;
+  @Input() user;
   @Output() onResponse = new EventEmitter<number>();
-  applications = [];
 
   constructor(private applicationService: ApplicationService,
               private responseHandler: ResponseHandlerService) {
   }
 
   ngOnInit() {
-    this.getApplications();
   }
 
-  getApplications() {
-    this.applicationService.getApplications(this.project_id)
-      .subscribe(
-        data => {
-          this.applications = data;
-            console.log(data,"applications")
-        },
-        error => this.responseHandler.errorMessage('An error occured!', error));
-  }
 
   acceptApplication(application_id, index) {
-    this.applicationService.acceptApplication(application_id)
-      .subscribe(
-        () => {
-          this.applications.splice(index, 1);
-          this.onResponse.emit(application_id);
-        },
-        error => this.responseHandler.errorMessage('An error occured!', error));
+    // this.applicationService.acceptApplication(application_id)
+    //   .subscribe(
+    //     () => {
+    //       this.applications.splice(index, 1);
+    //       this.onResponse.emit(application_id);
+    //     },
+    //     error => this.responseHandler.errorMessage('An error occured!', error));
   }
 
   declineApplication(application_id, index) {
-    this.applicationService.declineApplication(application_id)
-      .subscribe(
-        () => {
-          this.applications.splice(index, 1);
-          this.onResponse.emit(application_id);
-        },
-        error => this.responseHandler.errorMessage('An error occured!', error));
-  }
+    // this.applicationService.declineApplication(application_id)
+    //   .subscribe(
+    //     () => {
+    //       this.applications.splice(index, 1);
+    //       this.onResponse.emit(application_id);
+    //     },
+    //     error => this.responseHandler.errorMessage('An error occured!', error));
+   }
 }
--- src/app/projects/project-detail/project-applicants/project-applicants.component.ts
+++ src/app/projects/project-detail/project-applicants/project-applicants.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit, Input} from '@angular/core';
+import {ApplicationService} from '../../shared/application.service';
+import {ResponseHandlerService} from '../../../shared/response-handler.service';
+import {ProjectService} from '../../shared/project.service';
+import {Router} from '@angular/router';
+import {StateService} from '../../../shared/state.service';
 
 @Component({
   selector: 'app-project-applicants',
@@ -8,11 +13,53 @@ import {Component, OnInit, Input} from '@angular/core';
 export class ProjectApplicantsComponent implements OnInit {
 
   @Input() project_id;
+  private user;
+  private myProject = false;
+  private members = [];
+  private applying = [];
 
-  constructor() { }
-
-  ngOnInit() {
-
+  constructor(private applicationService: ApplicationService,
+              private projectService: ProjectService,
+              private state: StateService,
+              private router: Router,
+              private responseHandler: ResponseHandlerService) {
   }
 
+  ngOnInit() {
+      this.getProject();
+      this.getApplications();
+  }
+
+  // TODO just get the user not the whole project
+  getProject() {
+    this.projectService.getProjectById(this.project_id)
+      .subscribe(
+        project => {
+          if (project.data.length === 0) {
+            this.router.navigate(['/not-found']);
+            return;
+          }
+          this.user = project.data[0].user;
+
+          if (this.state.isLoggedIn()) {
+            this.verifyIfMyProject();
+          }
+        },
+        error => this.responseHandler.errorMessage('An error occured!', error));
+  }
+
+  verifyIfMyProject() {
+    this.myProject = this.state.getUser().id === this.user.id;
+  }
+
+  getApplications() {
+    this.applicationService.getApplications(this.project_id)
+      .subscribe(
+        data => {
+          data.map(object => {
+            object.accepted === true ? this.members.push(object) : this.applying.push(object);
+          });
+        },
+        error => this.responseHandler.errorMessage('An error occured!', error));
+  }
 }
