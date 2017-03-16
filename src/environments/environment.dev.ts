0692bde94b734ae2e61052ba19f82023b2fa63a6
Utwo <mihai.legat@gmail.com>
2017-03-17 00:34:00 +0200

    Compress image and replace favicons with new logo

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
   URL: 'http://localhost:4200/',
-  URL_API: 'http://startupper-utwo.rhcloud.com/',
+  // URL_API: 'http://startupper-utwo.rhcloud.com/',
+  URL_API: 'http://startup-back.dev:8000/',
   API_VERSION: 'api/v1/',
   FACEBOOK_ID: '1614652362188367',
   GITHUB_ID: 'fe2cc437add1417665e4',
