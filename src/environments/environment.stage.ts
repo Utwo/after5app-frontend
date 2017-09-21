4179e12d9a8f03947b2d2ba436ee833a265cf569
Utwo <mihai.legat@gmail.com>
2017-09-22 01:32:07 +0300

    Update stage url env.

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  URL: 'http://stage.after5app.co/',
-  URL_API: 'http://startupper-utwo.rhcloud.com/',
+  URL: 'https://stage.after5app.co/',
+  URL_API: 'https://startupper-utwo.rhcloud.com/',
   API_VERSION: 'api/v1/',
   FACEBOOK_ID: '1614652362188367',
   GITHUB_ID: 'fe2cc437add1417665e4',
