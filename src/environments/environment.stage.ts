82c876fe98c627aae2a1c10a230fe836e449e3c9
Oana Muntean <oanamuntean8@gmail.com>
2017-12-07 17:08:49 +0200

    Fix environment variables

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   URL: 'https://stage.after5app.co/',
-  URL_API: 'http://apistage.after5app.co/',
+  URL_API: 'https://apistage.after5app.co/',
   API_VERSION: 'api/v1/',
   FACEBOOK_ID: '1614652362188367',
   GITHUB_ID: 'fe2cc437add1417665e4'
