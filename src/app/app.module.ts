132a0b9864a49bdd00f811d0eb5ae10f018b46b6
Utwo <mihai.legat@gmail.com>
2017-12-09 13:21:00 +0200

    Move favicon to assets. Change ngsw-config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { ProjectsModule } from './projects/projects.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker'; 
+import { environment } from '../environments/environment'; 
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { HttpClientModule } from '@angular/common/http';
     ToastyModule.forRoot(),
     CoreModule.forRoot(),
     SharedModule,
-    ProjectsModule
+    ProjectsModule,
+    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
   ],
   providers: [
     appRoutingProviders,
