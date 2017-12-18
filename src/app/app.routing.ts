14bcaa144c1a77ece59c5997b38cdbff7e96d134
Oana Muntean <oanamuntean8@gmail.com>
2017-12-18 17:59:01 +0200

    Edit project module

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -14,6 +14,7 @@ const appRoutes: Routes = [
     loadChildren: './projects-overview/projects-overview.module#ProjectsOverviewModule',
     canActivate: [AuthGuard]
   },
+  {path: 'edit-project/:id', loadChildren: './edit-project/edit-project.module#EditProjectModule', canActivate: [AuthGuard]},
   {path: 'about', loadChildren: './about/about.module#AboutModule'},
   {path: 'start-project', loadChildren: './start-project/start-project.module#StartProjectModule'},
   {path: 'auth/:type/callback', loadChildren: './login/login.module#LoginModule'},
